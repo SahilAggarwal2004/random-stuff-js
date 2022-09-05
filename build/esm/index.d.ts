9fae3aa06f2ab0120e186b7a04e258c7b38ef9ff
Sahil <aggarwalsahil2004@gmail.com>
2022-09-05 21:43:13 +0530

    Initial commit

--- build/esm/index.d.ts
+++ build/esm/index.d.ts
@@ -1,2 +1,7 @@
+declare const randomNumber: (min: number, max: number) => number;
+declare const randomElement: (array: any[]) => any;
+declare const randomAdjective: () => string;
+declare const randomAnimal: () => string;
+declare const randomName: (separator?: string) => string;
 declare function generateOTP(digits?: number): string;
-export { generateOTP };
+export { randomNumber, randomElement, randomAdjective, randomAnimal, randomName, generateOTP };
--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,22 @@ const adjectives = ["Amazing", "Beautiful", "Calm", "Dangerous", "Enormous", "Fa
 // 200 animals
 const animals = ["Albatross", "Alligator", "Ant", "Antelope", "Ape", "Baboon", "Barracuda", "Bat", "Bear", "Beaver", "Bee", "Bison", "Boar", "Buffalo", "Butterfly", "Camel", "Cat", "Caterpillar", "Cattle", "Cheetah", "Chicken", "Chimpanzee", "Clam", "Cobra", "Crab", "Crane", "Crocodile", "Crow", "Curlew", "Deer", "Dinosaur", "Dog", "Dolphin", "Donkey", "Dotterel", "Dove", "Dragonfly", "Duck", "Dugong", "Dunlin", "Eagle", "Elephant", "Emu", "Falcon", "Ferret", "Finch", "Fish", "Flamingo", "Fly", "Fox", "Frog", "Gaur", "Gazelle", "Gerbil", "Giraffe", "Gnat", "Gnu", "Goat", "Goldfinch", "Goldfish", "Goose", "Gorilla", "Goshawk", "Grasshopper", "Grouse", "Guanaco", "Gull", "Hamster", "Hare", "Hawk", "Hedgehog", "Heron", "Herring", "Hippopotamus", "Hornet", "Horse", "Human", "Hummingbird", "Hyena", "Ibex", "Ibis", "Jackal", "Jaguar", "Jay", "Jellyfish", "Kangaroo", "Kingfisher", "Koala", "Kookabura", "Kouprey", "Kudu", "Lapwing", "Lark", "Lemur", "Leopard", "Lion", "Llama", "Lobster", "Locust", "Loris", "Louse", "Lyrebird", "Magpie", "Mallard", "Manatee", "Mandrill", "Mantis", "Marten", "Meerkat", "Mink", "Mole", "Mongoose", "Monkey", "Moose", "Mosquito", "Mouse", "Mule", "Narwhal", "Newt", "Nightingale", "Octopus", "Okapi", "Opossum", "Oryx", "Ostrich", "Otter", "Owl", "Oyster", "Panther", "Parrot", "Partridge", "Peafowl", "Pelican", "Penguin", "Pheasant", "Pig", "Pigeon", "Pony", "Porcupine", "Porpoise", "Quail", "Quelea", "Quetzal", "Rabbit", "Raccoon", "Rail", "Ram", "Rat", "Raven", "Red deer", "Red panda", "Reindeer", "Rhinoceros", "Rook", "Salamander", "Salmon", "Sand Dollar", "Sandpiper", "Sardine", "Scorpion", "Seahorse", "Seal", "Shark", "Sheep", "Shrew", "Skunk", "Snail", "Snake", "Sparrow", "Spider", "Spoonbill", "Squid", "Squirrel", "Starling", "Stingray", "Swallow", "Swan", "Termite", "Tiger", "Toad", "Trout", "Turkey", "Turtle", "Viper", "Vulture", "Wallaby", "Walrus", "Wasp", "Weasel", "Whale", "Wildcat", "Wolf", "Wolverine", "Wombat", "Woodcock", "Woodpecker", "Worm", "Wren", "Yak", "Zebra"]
 
-const randomElement = (array: any[]) => array[Math.floor(Math.random() * array.length)]
-const randomAdjective = (): string => adjectives[Math.floor(Math.random() * adjectives.length)]
-const randomAnimal = (): string => animals[Math.floor(Math.random() * animals.length)]
-const randomName = (separator: string = ' '): string => `${randomElement(adjectives)}${separator}${randomElement(animals)}`
+const randomNumber = (min: number, max: number): number => min + Math.floor(Math.random() * max)
+
+const randomElement = (array: any[]) => array[randomNumber(0, array.length)]
+
+const randomAdjective = (): string => randomElement(adjectives)
+
+const randomAnimal = (): string => randomElement(animals)
+
+const randomName = (separator: string = ' '): string => `${randomAdjective()}${separator}${randomAnimal()}`
 
 function generateOTP(digits: number = 4): string {
     digits = Math.min(digits, 20)
     const max = +('9'.repeat(digits))
-    let number = Math.floor(Math.random() * max).toString()
+    let number = randomNumber(0, max).toString()
     number = '0'.repeat(digits - number.length) + number
     return number
 }
 
-export { generateOTP }
+export { randomNumber, randomElement, randomAdjective, randomAnimal, randomName, generateOTP }
