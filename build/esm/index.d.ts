625dd2634d147da6b50a54e80b44b1dd00234d10
Sahil <aggarwalsahil2004@gmail.com>
2022-10-19 21:31:16 +0530

    added maximumNumber

--- build/esm/index.d.ts
+++ build/esm/index.d.ts
@@ -1,7 +1,8 @@
-declare const randomNumber: (min: number, max: number) => number;
+declare const randomNumber: (min?: number, max?: number) => number;
+declare const maximumNumber: (array: any[]) => number;
 declare const randomElement: (array: any[]) => any;
 declare const randomAdjective: () => string;
 declare const randomAnimal: () => string;
 declare const randomName: (separator?: string) => string;
 declare function generateOTP(digits?: number): string;
-export { randomNumber, randomElement, randomAdjective, randomAnimal, randomName, generateOTP };
+export { randomNumber, maximumNumber, randomElement, randomAdjective, randomAnimal, randomName, generateOTP };
