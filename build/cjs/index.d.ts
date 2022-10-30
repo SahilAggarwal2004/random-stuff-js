91667ebaa7efbaf333fb74dd6b4ce898885032cb
Sahil <aggarwalsahil2004@gmail.com>
2022-10-30 11:27:53 +0530

    added minimumNumber

--- build/cjs/index.d.ts
+++ build/cjs/index.d.ts
@@ -1,8 +1,9 @@
 declare const randomNumber: (min?: number, max?: number) => number;
-declare const maximumNumber: (array: any[]) => number;
+declare const minimumNumber: (array: number[]) => number;
+declare const maximumNumber: (array: number[]) => number;
 declare const randomElement: (array: any[]) => any;
 declare const randomAdjective: () => string;
 declare const randomAnimal: () => string;
 declare const randomName: (separator?: string) => string;
 declare function generateOTP(digits?: number): string;
-export { randomNumber, maximumNumber, randomElement, randomAdjective, randomAnimal, randomName, generateOTP };
+export { randomNumber, minimumNumber, maximumNumber, randomElement, randomAdjective, randomAnimal, randomName, generateOTP };
