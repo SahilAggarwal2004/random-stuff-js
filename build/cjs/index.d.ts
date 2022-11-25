a720a57181a800d0e3d8a2f0f8bd2d8d646eb52b
Sahil <aggarwalsahil2004@gmail.com>
2022-11-25 11:50:39 +0530

    added probability

--- build/cjs/index.d.ts
+++ build/cjs/index.d.ts
@@ -1,3 +1,4 @@
+declare const probability: (p: number) => boolean;
 declare const randomNumber: (min?: number, max?: number) => number;
 declare const minimumNumber: (array: number[]) => number;
 declare const maximumNumber: (array: number[]) => number;
@@ -6,4 +7,4 @@ declare const randomAdjective: () => string;
 declare const randomAnimal: () => string;
 declare const randomName: (separator?: string) => string;
 declare function generateOTP(digits?: number): string;
-export { randomNumber, minimumNumber, maximumNumber, randomElement, randomAdjective, randomAnimal, randomName, generateOTP };
+export { probability, randomNumber, minimumNumber, maximumNumber, randomElement, randomAdjective, randomAnimal, randomName, generateOTP };
